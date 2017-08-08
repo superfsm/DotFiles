d3d849b09cafb889d918588a913bef70e780bae9
Shaoming Feng <frank@houzz.com>
2017-08-08 13:44:59 -0700

    update check, gitignore

--- collect.sh
+++ collect.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
 cp -r ~/.oh-my-zsh/custom oh-my-zsh/
+cp ~/.gitconfig .gitconfig
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
 cp -rT oh-my-zsh/* ~/.oh-my-zsh/custom/
+cp .gitconfig ~/.gitconfig
